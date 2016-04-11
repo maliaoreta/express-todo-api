6da53c9e75ad876bbb2134082d0173178aa4d1ad
maliaoreta <maliaoreta@gmail.com>
2016-04-11 12:31:59 -1000

    validates the reset value

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
     score = require('./routes/score.js'),
-    buzzwordArr = require('./routes/buzzwordContents');
-
+    buzzwordArr = require('./routes/buzzwordContents'),
+    validations = require('./routes/validations.js');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -19,14 +19,21 @@ app.get('/buzzwords', function (req, res) {
     buzzWords: buzzwordArr
   })
 })
-.post('/reset', function (req, res) {
+.post('/reset', validations(['reset']), function (req, res) {
 
-  score = 0;
-  buzzwordArr.splice(0);
-  return res.json({
-    success: true
-  })
-})
+    if (req.body.reset === 'true') {
+
+      score = 0;
+      buzzwordArr.splice(0);
+      return res.json({
+        success: true
+      })
+    }
+    else {
+      return res.send('To reset the buzzword collection, set "reset" to true!');
+    }
+  }
+)
 .use('/buzzword', buzzwordsRoute);
 
 var server = app.listen(8080, function () {
