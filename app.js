afe80c216d1890343bf394c369e901280cdba6ee
maliaoreta <maliaoreta@gmail.com>
2016-04-11 19:14:10 -1000

    separates all request handling into routers

--- app.js
+++ app.js
@@ -1,45 +1,28 @@
 var express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
-    score = require('./routes/score.js'),
-    buzzwordArr = require('./routes/buzzwordContents'),
-    validations = require('./routes/validations.js');
+    score = require('./model/score.js'),
+    buzzwordArr = require('./model/buzzwordContents.js'),
+    validations = require('./middleware/validations.js'),
+    buzzwordsRoute = require('./routes/buzzwords'),
+    rootRoute = require('./routes/rootRoute.js');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-// /buzzword route
-var buzzwordsRoute = require('./routes/buzzwords');
 
-app.use(express.static('public'));
+app.use(express.static('public'))
+  .use('/', rootRoute)
+  .use('/buzzword', buzzwordsRoute);
 
-app.get('/buzzwords', function (req, res) {
-  
-   return res.json({
-    buzzWords: buzzwordArr
-  })
-})
-.post('/reset', validations(['reset']), function (req, res) {
+if(!module.parent){
 
-    if (req.body.reset === 'true') {
-
-      score = 0;
-      buzzwordArr.splice(0);
-      return res.json({
-        success: true
-      })
-    }
-    else {
-      return res.send('To reset the buzzword collection, set "reset" to true!');
-    }
-  }
-)
-.use('/buzzword', buzzwordsRoute);
-
-var server = app.listen(8080, function () {
-
+  var server = app.listen(8080, function () {
   var host = server.address().address;
   var port = server.address().port;
 
   console.log('Listening on 8080');
-});
+  });
+}
+
+module.exports = app;
